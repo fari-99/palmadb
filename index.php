adb54103721562eb31006de64a302184f140daae
Fadhlan Rizal <fadhlan.fari@gmail.com>
2015-10-05 12:16:48 +0700

    navigation dipisah

--- index.php
+++ index.php
@@ -29,46 +29,7 @@
 <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Home</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li class="dropdown">
-                        <a id="drop1" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 
-                            aria-expanded="false">
-                            Tools
-                            <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="drop1">
-                            <li><a href="uploadSNP.php">Upload SNP</a></li>
-                            <li><a href="uploadPRIMER.php">Upload Primer</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="searchPRIMER.php">Search Primer</a></li>
-                            <li><a href="searchSNP.php">Search SNP</a></li>
-                        </ul>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+    <?php include "nav.php";?>
 
     <!-- Page Content -->
     <div class="container">
